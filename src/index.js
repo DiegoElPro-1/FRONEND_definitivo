d819116275680c773bb01838b96ccad7a00d2bfb
Angie Escobar <escobarav103@gmail.com>
2026-05-28 00:59:26 -0500

    agregando import

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
+import { Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
