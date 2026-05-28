4056efe98678fdfd4bee7f2fb8d8b6392fc11695
Angie Escobar <escobarav103@gmail.com>
2026-05-28 01:36:00 -0500

    borrando import

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
-import { Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
